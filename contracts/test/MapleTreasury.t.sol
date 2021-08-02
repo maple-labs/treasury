19b4c947fa1817c440bc8fd602990e402570cfd2
SatyamAgrawal <satyam0499@gmail.com>
2021-08-02 16:28:07 +0530

    fix test cases

--- contracts/test/MapleTreasury.t.sol
+++ contracts/test/MapleTreasury.t.sol
@@ -30,6 +30,34 @@ contract MapleToken is ERC20 {
 
 }
 
+interface Hevm {
+
+    function warp(uint256) external;
+    function store(address,bytes32,bytes32) external;
+
+}
+
+interface IBasicFDT {
+    /**
+        @dev Withdraws all available funds for the calling FDT holder.
+     */
+    function withdrawFunds() external;
+}
+
+interface IMapleGlobalsLike {
+
+    function getLatestPrice(address asset) external view returns (uint256);
+
+}
+
+contract Holder {
+
+    function withdrawFunds(address token) external {
+        IBasicFDT(token).withdrawFunds();
+    }
+
+}
+
 contract MapleTreasuryTest is DSTest {
 
     Governor               realGov; 
@@ -38,9 +66,25 @@ contract MapleTreasuryTest is DSTest {
     MapleGlobals           globals;
     GlobalAdmin    realGlobalAdmin;
     MapleToken                 mpl;
+    Hevm                      hevm;
+    Holder                     hal;
+    Holder                     hue;
+
+    constructor() public { hevm = Hevm(address(bytes20(uint160(uint256(keccak256("hevm cheat code")))))); }
 
     address constant UNISWAP_V2_ROUTER_02 = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;  // Uniswap V2 Router
     address constant USDC                 = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address constant DAI                  = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address constant WETH                 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address constant WBTC                 = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+    address constant wethOracle           = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address constant wbtcOracle           = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+    address constant usdcOracle           = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    uint256 constant USD                  = 10 ** 6;  // USDC precision decimals
+    uint256 constant BTC                  = 10 ** 8;  // WBTC precision decimals
+    uint256 constant WAD                  = 10 ** 18;
+    uint256 constant RAY                  = 10 ** 27;
+
 
     function setUp() public {
 
@@ -50,126 +94,159 @@ contract MapleTreasuryTest is DSTest {
         mpl             = new MapleToken("Maple", "MPL");
         globals         = new MapleGlobals(address(realGov), address(mpl), address(realGlobalAdmin));
         treasury        = new MapleTreasury(address(mpl), USDC, UNISWAP_V2_ROUTER_02, address(globals));
-
-        log_named_address("Treasury global address", treasury.globals());
+        hal             = new Holder();
+        hue             = new Holder();
         
-        // mint("WBTC", address(this),  10 * BTC);
-        // mint("WETH", address(this),  10 ether);
-        // mint("DAI",  address(this), 100 ether);
-        // mint("USDC", address(this), 100 * USD);
+        mint(WBTC, address(this),  10 * BTC, 0);
+        mint(WETH, address(this),  10 ether, 3);
+        mint(DAI,  address(this), 100 ether, 2);
+        mint(USDC, address(this), 100 * USD, 9);
+
+        mpl.mint(address(this), 10000000 * WAD);
+
+        realGov.setPriceOracle(address(globals), WETH, wethOracle);
+        realGov.setPriceOracle(address(globals), WBTC, wbtcOracle);
+        realGov.setPriceOracle(address(globals), DAI,  usdcOracle);
     }
 
     function test_setGlobals() public {
         IMapleGlobals globals2 = fakeGov.createGlobals(address(mpl));               // Create upgraded MapleGlobals
         assertEq(address(treasury.globals()), address(globals));
 
-        // assertTrue(!fakeGov.try_setGlobals(address(treasury), address(globals2)));  // Non-governor cannot set new globals
+        assertTrue(!fakeGov.try_setGlobals(address(treasury), address(globals2)));  // Non-governor cannot set new globals
 
-        // globals2 = realGov.createGlobals(address(mpl));                             // Create upgraded MapleGlobals
+        globals2 = realGov.createGlobals(address(mpl));                             // Create upgraded MapleGlobals
 
-        // assertTrue(realGov.try_setGlobals(address(treasury), address(globals2)));   // Governor can set new globals
-        // assertEq(address(treasury.globals()), address(globals2));                   // Globals is updated
+        assertTrue(realGov.try_setGlobals(address(treasury), address(globals2)));   // Governor can set new globals
+        assertEq(address(treasury.globals()), address(globals2));                   // Globals is updated
     }
 
-    // function test_withdrawFunds() public {
-    //     assertEq(IERC20(USDC).balanceOf(address(treasury)), 0);
+    function test_withdrawFunds() public {
+        assertEq(IERC20(USDC).balanceOf(address(treasury)), 0);
 
-    //     IERC20(USDC).transfer(address(treasury), 100 * USD);
+        IERC20(USDC).transfer(address(treasury), 100 * USD);
 
-    //     assertEq(IERC20(USDC).balanceOf(address(treasury)), 100 * USD);
-    //     assertEq(IERC20(USDC).balanceOf(address(gov)),         0);
+        assertEq(IERC20(USDC).balanceOf(address(treasury)), 100 * USD);
+        assertEq(IERC20(USDC).balanceOf(address(realGov)),          0);
+        assertEq(treasury.globals(), address(globals));
 
-    //     assertTrue(!fakeGov.try_reclaimERC20_treasury(USDC, 40 * USD));  // Non-governor can't withdraw
-    //     assertTrue(     gov.try_reclaimERC20_treasury(USDC, 40 * USD));
+        assertTrue(!fakeGov.try_reclaimERC20_treasury(address(treasury), USDC, 40 * USD));  // Non-governor can't withdraw
+        assertTrue( realGov.try_reclaimERC20_treasury(address(treasury), USDC, 40 * USD));
 
-    //     assertEq(IERC20(USDC).balanceOf(address(treasury)), 60 * USD);  // Can be distributed to MPL holders
-    //     assertEq(IERC20(USDC).balanceOf(address(gov)), 40 * USD);  // Withdrawn to MapleDAO address for funding
-    // }
+        assertEq(IERC20(USDC).balanceOf(address(treasury)), 60 * USD);  // Can be distributed to MPL holders
+        assertEq(IERC20(USDC).balanceOf(address(realGov)),  40 * USD);  // Withdrawn to MapleDAO address for funding
+    }
 
-    // function test_distributeToHolders() public {
-    //     assertEq(mpl.balanceOf(address(hal)), 0);
-    //     assertEq(mpl.balanceOf(address(hue)), 0);
+    function test_distributeToHolders() public {
+        assertEq(mpl.balanceOf(address(hal)), 0);
+        assertEq(mpl.balanceOf(address(hue)), 0);
 
-    //     mpl.transfer(address(hal), mpl.totalSupply() * 25 / 100);  // 25%
-    //     mpl.transfer(address(hue), mpl.totalSupply() * 75 / 100);  // 75%
+        mpl.transfer(address(hal), mpl.totalSupply() * 25 / 100);  // 25%
+        mpl.transfer(address(hue), mpl.totalSupply() * 75 / 100);  // 75%
 
-    //     assertEq(mpl.balanceOf(address(hal)), 2_500_000 ether);
-    //     assertEq(mpl.balanceOf(address(hue)), 7_500_000 ether);
+        assertEq(mpl.balanceOf(address(hal)), 2_500_000 ether);
+        assertEq(mpl.balanceOf(address(hue)), 7_500_000 ether);
 
-    //     assertEq(IERC20(USDC).balanceOf(address(treasury)), 0);
+        assertEq(IERC20(USDC).balanceOf(address(treasury)), 0);
 
-    //     IERC20(USDC).transfer(address(treasury), 100 * USD);
+        IERC20(USDC).transfer(address(treasury), 100 * USD);
 
-    //     assertEq(IERC20(USDC).balanceOf(address(treasury)), 100 * USD);
-    //     assertEq(IERC20(USDC).balanceOf(address(mpl)),              0);
+        assertEq(IERC20(USDC).balanceOf(address(treasury)), 100 * USD);
+        assertEq(IERC20(USDC).balanceOf(address(mpl)),              0);
 
-    //     assertTrue(!fakeGov.try_distributeToHolders());  // Non-governor can't distribute
-    //     assertTrue(     gov.try_distributeToHolders());  // Governor can distribute
+        assertTrue(!fakeGov.try_distributeToHolders(address(treasury)));  // Non-governor can't distribute
+        assertTrue( realGov.try_distributeToHolders(address(treasury)));  // Governor can distribute
 
-    //     assertEq(IERC20(USDC).balanceOf(address(treasury)),         0);  // Withdraws all funds
-    //     assertEq(IERC20(USDC).balanceOf(address(mpl)),      100 * USD);  // Withdrawn to MPL address, where accounts can claim funds
+        assertEq(IERC20(USDC).balanceOf(address(treasury)),         0);  // Withdraws all funds
+        assertEq(IERC20(USDC).balanceOf(address(mpl)),      100 * USD);  // Withdrawn to MPL address, where accounts can claim funds
 
-    //     assertEq(IERC20(USDC).balanceOf(address(hal)), 0);  // Token holder hasn't claimed
-    //     assertEq(IERC20(USDC).balanceOf(address(hue)), 0);  // Token holder hasn't claimed
+        assertEq(IERC20(USDC).balanceOf(address(hal)), 0);  // Token holder hasn't claimed
+        assertEq(IERC20(USDC).balanceOf(address(hue)), 0);  // Token holder hasn't claimed
+    }
 
-    //     hal.withdrawFunds(address(mpl));
-    //     hue.withdrawFunds(address(mpl));
+    function test_convertERC20() public {
 
-    //     withinDiff(IERC20(USDC).balanceOf(address(hal)), 25 * USD, 1);  // Token holder has claimed proportional share of USDC
-    //     withinDiff(IERC20(USDC).balanceOf(address(hue)), 75 * USD, 1);  // Token holder has claimed proportional share of USDC
-    // }
+        IMapleGlobalsLike _globals = IMapleGlobalsLike(address(globals));
 
-    // function test_convertERC20() public {
+        assertEq(IERC20(WBTC).balanceOf(address(treasury)), 0);
+        assertEq(IERC20(WETH).balanceOf(address(treasury)), 0);
+        assertEq(IERC20(DAI).balanceOf(address(treasury)),  0);
 
-    //     IMapleGlobals _globals = IMapleGlobals(address(globals));
+        IERC20(WBTC).transfer(address(treasury), 10 * BTC);
+        IERC20(WETH).transfer(address(treasury), 10 ether);
+        IERC20(DAI).transfer(address(treasury), 100 ether);
 
-    //     assertEq(IERC20(WBTC).balanceOf(address(treasury)), 0);
-    //     assertEq(IERC20(WETH).balanceOf(address(treasury)), 0);
-    //     assertEq(IERC20(DAI).balanceOf(address(treasury)),  0);
+        assertEq(IERC20(WBTC).balanceOf(address(treasury)),  10 * BTC);
+        assertEq(IERC20(WETH).balanceOf(address(treasury)),  10 ether);
+        assertEq(IERC20(DAI).balanceOf(address(treasury)),  100 ether);
+        assertEq(IERC20(USDC).balanceOf(address(treasury)),         0);
 
-    //     IERC20(WBTC).transfer(address(treasury), 10 * BTC);
-    //     IERC20(WETH).transfer(address(treasury), 10 ether);
-    //     IERC20(DAI).transfer(address(treasury), 100 ether);
+        uint256 expectedAmtFromWBTC = Util.calcMinAmount(_globals, WBTC, USDC,  10 * BTC);
+        uint256 expectedAmtFromWETH = Util.calcMinAmount(_globals, WETH, USDC,  10 ether);
+        uint256 expectedAmtFromDAI  = Util.calcMinAmount(_globals, DAI,  USDC, 100 ether);
 
-    //     assertEq(IERC20(WBTC).balanceOf(address(treasury)),  10 * BTC);
-    //     assertEq(IERC20(WETH).balanceOf(address(treasury)),  10 ether);
-    //     assertEq(IERC20(DAI).balanceOf(address(treasury)),  100 ether);
-    //     assertEq(IERC20(USDC).balanceOf(address(treasury)),         0);
+        /*** Convert WBTC ***/
+        assertTrue(!fakeGov.try_convertERC20(address(treasury), WBTC));  // Non-governor can't convert
+        assertTrue( realGov.try_convertERC20(address(treasury), WBTC));  // Governor can convert
 
-    //     uint256 expectedAmtFromWBTC = Util.calcMinAmount(_globals, WBTC, USDC,  10 * BTC);
-    //     uint256 expectedAmtFromWETH = Util.calcMinAmount(_globals, WETH, USDC,  10 ether);
-    //     uint256 expectedAmtFromDAI  = Util.calcMinAmount(_globals, DAI,  USDC, 100 ether);
+        assertEq(IERC20(WBTC).balanceOf(address(treasury)),         0);
+        assertEq(IERC20(DAI).balanceOf(address(treasury)),  100 ether);
 
-    //     /*** Convert WBTC ***/
-    //     assertTrue(!fakeGov.try_convertERC20(WBTC));  // Non-governor can't convert
-    //     assertTrue(     gov.try_convertERC20(WBTC));  // Governor can convert
+        withinPercentage(IERC20(USDC).balanceOf(address(treasury)), expectedAmtFromWBTC, 300);  // Less than 3% difference
 
-    //     assertEq(IERC20(WBTC).balanceOf(address(treasury)),         0);
-    //     assertEq(IERC20(DAI).balanceOf(address(treasury)),  100 ether);
+        realGov.distributeToHolders(IMapleTreasury(address(treasury)));  // Empty treasury balance of USDC
 
-    //     withinPercentage(IERC20(USDC).balanceOf(address(treasury)), expectedAmtFromWBTC, 300);  // Less than 3% difference
+        /*** Convert WETH ***/
+        assertTrue(!fakeGov.try_convertERC20(address(treasury), WETH));  // Non-governor can't convert
+        assertTrue( realGov.try_convertERC20(address(treasury), WETH));  // Governor can convert
 
-    //     gov.distributeToHolders();  // Empty treasury balance of USDC
+        assertEq(IERC20(WETH).balanceOf(address(treasury)),         0);
+        assertEq(IERC20(DAI).balanceOf(address(treasury)),  100 ether);
 
-    //     /*** Convert WETH ***/
-    //     assertTrue(!fakeGov.try_convertERC20(WETH));  // Non-governor can't convert
-    //     assertTrue(     gov.try_convertERC20(WETH));  // Governor can convert
+        withinPercentage(IERC20(USDC).balanceOf(address(treasury)), expectedAmtFromWETH, 300);  // Less than 3% difference
 
-    //     assertEq(IERC20(WETH).balanceOf(address(treasury)),         0);
-    //     assertEq(IERC20(DAI).balanceOf(address(treasury)),  100 ether);
+        realGov.distributeToHolders(IMapleTreasury(address(treasury)));  // Empty treasury balance of USDC
 
-    //     withinPercentage(IERC20(USDC).balanceOf(address(treasury)), expectedAmtFromWETH, 300);  // Less than 3% difference
+        /*** Convert DAI ***/
+        assertTrue(!fakeGov.try_convertERC20(address(treasury), DAI));  // Non-governor can't convert
+        assertTrue( realGov.try_convertERC20(address(treasury), DAI));  // Governor can convert
 
-    //     gov.distributeToHolders();  // Empty treasury balance of USDC
+        assertEq(IERC20(WETH).balanceOf(address(treasury)), 0);
+        assertEq(IERC20(DAI).balanceOf(address(treasury)),  0);
 
-    //     /*** Convert DAI ***/
-    //     assertTrue(!fakeGov.try_convertERC20(DAI));  // Non-governor can't convert
-    //     assertTrue(     gov.try_convertERC20(DAI));  // Governor can convert
+        withinPercentage(IERC20(USDC).balanceOf(address(treasury)), expectedAmtFromDAI, 300);  // Less than 3% difference
+    }
 
-    //     assertEq(IERC20(WETH).balanceOf(address(treasury)), 0);
-    //     assertEq(IERC20(DAI).balanceOf(address(treasury)),  0);
+     // Manipulate mainnet ERC20 balance
+    function mint(address addr, address account, uint256 amt, uint256 slot) public {
+        uint256 bal = IERC20(addr).balanceOf(account);
 
-    //     withinPercentage(IERC20(USDC).balanceOf(address(treasury)), expectedAmtFromDAI, 300);  // Less than 3% difference
-    // }
+        hevm.store(
+            addr,
+            keccak256(abi.encode(account, slot)), // Mint tokens
+            bytes32(bal + amt)
+        );
 
+        assertEq(IERC20(addr).balanceOf(account), bal + amt, "Balance slot is wrong");  // Assert new balance
+    }
+
+    // Verify equality within accuracy percentage (basis points)
+    function withinPercentage(uint256 val0, uint256 val1, uint256 percentage) public {
+        uint256 diff = getDiff(val0, val1);
+        if (diff == 0) return;
+
+        uint256 denominator = val0 == 0 ? val1 : val0;
+        bool check = ((diff * RAY) / denominator) < percentage * RAY / 10_000;
+
+        if (check) return;
+
+        emit log_named_uint("Error: approx a == b not satisfied, accuracy digits ", percentage);
+        emit log_named_uint("  Expected", val0);
+        emit log_named_uint("    Actual", val1);
+        fail();
+    }
+
+    function getDiff(uint256 val0, uint256 val1) internal pure returns (uint256 diff) {
+        diff = val0 > val1 ? val0 - val1 : val1 - val0;
+    }
 }
--- contracts/test/accounts/Governor.sol
+++ contracts/test/accounts/Governor.sol
@@ -2,10 +2,11 @@
 pragma solidity 0.6.11;
 pragma experimental ABIEncoderV2;
 
-import { IERC20 } from "../../../modules/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-
+import { IERC20 }        from "../../../modules/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import { IMapleGlobals } from "../../../modules/globals/contracts/interfaces/IMapleGlobals.sol";
 import { MapleGlobals }  from "../../../modules/globals/contracts/MapleGlobals.sol";
+
+import { IMapleTreasury }  from "../../interfaces/IMapleTreasury.sol";
 
 contract Governor {
 
@@ -17,6 +18,7 @@ contract Governor {
     function setPriceOracle(address globals, address asset, address oracle)            external { IMapleGlobals(globals).setPriceOracle(asset, oracle); }
     function setValidSubFactory(address globals, address fac, address sub, bool valid) external { IMapleGlobals(globals).setValidSubFactory(fac, sub, valid); }
     function setCalc(address globals, address calc, bool valid)                        external { IMapleGlobals(globals).setCalc(calc, valid); }
+    function distributeToHolders(IMapleTreasury treasury)                              external { treasury.distributeToHolders(); }
 
     /*********************/
     /*** Try Functions ***/
@@ -149,6 +151,21 @@ contract Governor {
     function try_setGlobals(address target, address _globals) external returns (bool ok) {
         string memory sig = "setGlobals(address)";
         (ok,) = address(target).call(abi.encodeWithSignature(sig, _globals));
+    }
+
+    function try_reclaimERC20_treasury(address treasury, address asset, uint256 amount) external returns (bool ok) {
+        string memory sig = "reclaimERC20(address,uint256)";
+        (ok,) = address(treasury).call(abi.encodeWithSignature(sig, asset, amount));
+    }
+
+    function try_distributeToHolders(address treasury) external returns (bool ok) {
+        string memory sig = "distributeToHolders()";
+        (ok,) = address(treasury).call(abi.encodeWithSignature(sig));
+    }
+
+    function try_convertERC20(address treasury, address asset) external returns (bool ok) {
+        string memory sig = "convertERC20(address)";
+        (ok,) = address(treasury).call(abi.encodeWithSignature(sig, asset));
     }
 
     function createGlobals(address mpl) external returns (MapleGlobals globals) {

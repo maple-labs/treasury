c22ac2d7fa934b1d2e905504e3143e86112c39fe
Lucas <lucas@maple.finance>
2021-08-12 17:46:06 -0400

    feat: remove unecessary code, rename user functions

--- contracts/test/MapleTreasury.t.sol
+++ contracts/test/MapleTreasury.t.sol
@@ -5,8 +5,6 @@ import { DSTest }        from "../../modules/ds-test/src/test.sol";
 import { ERC20 }         from "../../modules/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 import { Governor }       from "./accounts/Governor.sol";
-import { GlobalAdmin }    from "./accounts/GlobalAdmin.sol";
-import { Holder }         from "./accounts/Holder.sol";
 
 import { MapleTreasury }  from "../MapleTreasury.sol";
 
@@ -32,9 +30,7 @@ contract TokenMock is ERC20 {
         _mint(to, amt);
     }
 
-    function updateFundsReceived() external {
-        // TODO: implementation
-    }
+    function updateFundsReceived() external { }
 
 }
 
@@ -45,25 +41,22 @@ contract MapleTreasuryTest is DSTest {
     MapleTreasury treasury;
     GlobalsMock   globals;
     TokenMock     mpl;
-    TokenMock     mock;
-    Holder        holder1;
-    Holder        holder2;
-    GlobalAdmin   realGlobalAdmin;
+    TokenMock     mockToken;
+
+    address constant holder1     = address(1);
+    address constant holder2     = address(2);
+    address constant globalAdmin = address(3);
 
     function setUp() public {
-
-        realGlobalAdmin = new GlobalAdmin();
         realGov         = new Governor();
         fakeGov         = new Governor();
         mpl             = new TokenMock("Maple",    "MPL");
-        mock            = new TokenMock("MK token", "MK");
-        globals         = new GlobalsMock(address(realGov), address(mpl), address(realGlobalAdmin));
-        treasury        = new MapleTreasury(address(mpl), address(mock), address(1), address(globals));
-        holder1         = new Holder();
-        holder2         = new Holder();
+        mockToken       = new TokenMock("MK token", "MK");
+        globals         = new GlobalsMock(address(realGov), address(mpl), address(globalAdmin));
+        treasury        = new MapleTreasury(address(mpl), address(mockToken), address(1), address(globals));
 
-        mpl.mint(address(this),  100);
-        mock.mint(address(this), 100);
+        mpl.mint(address(this),       100);
+        mockToken.mint(address(this), 100);
     }
 
     function test_setGlobals() public {
@@ -76,19 +69,19 @@ contract MapleTreasuryTest is DSTest {
     }
 
     function test_reclaimERC20() public {
-        assertEq(mock.balanceOf(address(treasury)), 0);
+        assertEq(mockToken.balanceOf(address(treasury)), 0);
 
-        mock.transfer(address(treasury), 100);
+        mockToken.transfer(address(treasury), 100);
 
-        assertEq(mock.balanceOf(address(treasury)), 100);
-        assertEq(mock.balanceOf(address(realGov)),  0);
+        assertEq(mockToken.balanceOf(address(treasury)), 100);
+        assertEq(mockToken.balanceOf(address(realGov)),  0);
         assertEq(treasury.globals(), address(globals));
 
-        assertTrue(!fakeGov.try_treasury_reclaimERC20(address(treasury), address(mock), 40));  // Non-governor can't withdraw
-        assertTrue( realGov.try_treasury_reclaimERC20(address(treasury), address(mock), 40));
+        assertTrue(!fakeGov.try_treasury_reclaimERC20(address(treasury), address(mockToken), 40));  // Non-governor can't withdraw
+        assertTrue( realGov.try_treasury_reclaimERC20(address(treasury), address(mockToken), 40));
 
-        assertEq(mock.balanceOf(address(treasury)), 60);  // Can be distributed to MPL holders
-        assertEq(mock.balanceOf(address(realGov)),  40);  // Withdrawn to MapleDAO address for funding
+        assertEq(mockToken.balanceOf(address(treasury)), 60);  // Can be distributed to MPL holders
+        assertEq(mockToken.balanceOf(address(realGov)),  40);  // Withdrawn to MapleDAO address for funding
     }
 
     function test_distributeToHolders() public {
@@ -101,20 +94,20 @@ contract MapleTreasuryTest is DSTest {
         assertEq(mpl.balanceOf(address(holder1)), 25);
         assertEq(mpl.balanceOf(address(holder2)), 75);
 
-        assertEq(mock.balanceOf(address(treasury)), 0);
+        assertEq(mockToken.balanceOf(address(treasury)), 0);
 
-        mock.transfer(address(treasury), 100);
+        mockToken.transfer(address(treasury), 100);
 
-        assertEq(mock.balanceOf(address(treasury)), 100);
-        assertEq(mock.balanceOf(address(mpl)),      0);
+        assertEq(mockToken.balanceOf(address(treasury)), 100);
+        assertEq(mockToken.balanceOf(address(mpl)),      0);
 
         assertTrue(!fakeGov.try_treasury_distributeToHolders(address(treasury)));  // Non-governor can't distribute
         assertTrue( realGov.try_treasury_distributeToHolders(address(treasury)));  // Governor can distribute
 
-        assertEq(mock.balanceOf(address(treasury)), 0);  // Withdraws all funds
-        assertEq(mock.balanceOf(address(mpl)),      100);  // Withdrawn to MPL address, where accounts can claim funds
-        assertEq(mock.balanceOf(address(holder1)),  0);  // Token holder hasn't claimed
-        assertEq(mock.balanceOf(address(holder2)),  0);  // Token holder hasn't claimed
+        assertEq(mockToken.balanceOf(address(treasury)), 0);    // Withdraws all funds
+        assertEq(mockToken.balanceOf(address(mpl)),      100);  // Withdrawn to MPL address, where accounts can claim funds
+        assertEq(mockToken.balanceOf(address(holder1)),  0);    // Token holder hasn't claimed
+        assertEq(mockToken.balanceOf(address(holder2)),  0);    // Token holder hasn't claimed
     }
 
     // TODO: Test remain for the `converERC20`, It would get added during integration tests.
--- contracts/test/accounts/Governor.sol
+++ contracts/test/accounts/Governor.sol
@@ -1,26 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.6.11;
 
-interface IMapleGlobalsLike {
-    function setPriceOracle(address, address) external;
-}
-
-interface IMapleTreasuryLike {
-    function distributeToHolders() external;
-}
-
 contract Governor {
-
-    /************************/
-    /*** Direct Functions ***/
-    /************************/
-    function setPriceOracle(address globals, address asset, address oracle) external {
-        IMapleGlobalsLike(globals).setPriceOracle(asset, oracle); 
-    }
-
-    function distributeToHolders(address treasury) external {
-        IMapleTreasuryLike(treasury).distributeToHolders(); 
-    }
 
     /*********************/
     /*** Try Functions ***/
@@ -38,11 +19,6 @@ contract Governor {
     function try_treasury_distributeToHolders(address treasury) external returns (bool ok) {
         string memory sig = "distributeToHolders()";
         (ok,) = treasury.call(abi.encodeWithSignature(sig));
-    }
-
-    function try_treasury_convertERC20(address treasury, address asset) external returns (bool ok) {
-        string memory sig = "convertERC20(address)";
-        (ok,) = treasury.call(abi.encodeWithSignature(sig, asset));
     }
 
 }
